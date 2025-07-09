490b9d3b973d479f6bd38ebd9832d8634e6a180c
GitFrog1111 <jonty.wainwright@gmail.com>
2025-07-10 02:43:23 +0700

    fully functional mobile and desktop

--- app.py
+++ app.py
@@ -354,7 +354,6 @@ def RenderDesktop():
             css_styles=cssstyles):
             st.container(border = False, height = upperpad)
             st.markdown(f"<img src='app/static/Images/Icons/StartToFluent.png' style='margin-top: 0px;'>", unsafe_allow_html=True)
-            st.container(border = False, height = lowerpad)
             
             st.markdown("<h1 style='text-align: left; font-size: 24px; font-weight: 600;'>From Start to Fluent</h1>", unsafe_allow_html=True)
             st.markdown(f"<p style='text-align: left; font-size: 16px; font-weight: 400; margin-top: -10px;'>{st.session_state.Features['From Start to Fluent']}</p>", unsafe_allow_html=True)
@@ -364,7 +363,7 @@ def RenderDesktop():
             css_styles=cssstyles):
             st.container(border = False, height = upperpad)
             st.markdown(f"<img src='app/static/Images/Icons/Missions.png' style='margin-top: 0px;'>", unsafe_allow_html=True)
-            st.container(border = False, height = lowerpad)
+
             st.markdown("<h1 style='text-align: left; font-size: 24px; font-weight: 600;'>Missions</h1>", unsafe_allow_html=True)
             st.markdown(f"<p style='text-align: left; font-size: 16px; font-weight: 400; margin-top: -10px;'>{st.session_state.Features['Missions']}</p>", unsafe_allow_html=True)
     with col3:
@@ -373,7 +372,7 @@ def RenderDesktop():
             css_styles=cssstyles):
             st.container(border = False, height = upperpad)
             st.markdown(f"<img src='app/static/Images/Icons/EndlessEncounters.png' style='margin-top: 0px;'>", unsafe_allow_html=True)
-            st.container(border = False, height = lowerpad)
+            
             st.markdown("<h1 style='text-align: left; font-size: 24px; font-weight: 600;'>Endless Encounters</h1>", unsafe_allow_html=True)
             st.markdown(f"<p style='text-align: left; font-size: 16px; font-weight: 400; margin-top: -10px;'>{st.session_state.Features['Endless Encounters']}</p>", unsafe_allow_html=True)
     with col4:
@@ -382,7 +381,7 @@ def RenderDesktop():
             css_styles=cssstyles):
             st.container(border = False, height = upperpad)
             st.markdown(f"<img src='app/static/Images/Icons/Leaderboard.png' style='margin-top: 0px;'>", unsafe_allow_html=True)
-            st.container(border = False, height = lowerpad)
+            
             st.markdown("<h1 style='text-align: left; font-size: 24px; font-weight: 600;'>Leaderboard</h1>", unsafe_allow_html=True)
             st.markdown(f"<p style='text-align: left; font-size: 16px; font-weight: 400; margin-top: -10px;'>{st.session_state.Features['Leaderboard']}</p>", unsafe_allow_html=True)
 
